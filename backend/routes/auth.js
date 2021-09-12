0617656949a74b2b8fb947ab63804ee445034535
TheAlpha27 <utsav.soni.27@gmail.com>
2021-09-12 19:11:17 +0530

    added express-validator check in auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 //create a user using: POST '/api/auth/'. Doesn't require Auth
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/',[
+    body('name', 'Enter a valid name').isLength({min: 3}),
+    body('email').isEmail(),
+    body('password').isLength({min: 5})
+],(req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email
+    }).then(user => res.json(user)).catch(err=>{console.log(err), res.json({error: 'Enter a unique value for email', message: err.message})});
 })
 
 module.exports= router;
