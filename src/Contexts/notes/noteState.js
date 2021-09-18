24709dc71b4952235c8b4a9b0ceb91ef4e0c4e70
TheAlpha27 <utsav.soni.27@gmail.com>
2021-09-19 02:47:55 +0530

    Understanding useLocation Hook

--- src/Contexts/notes/noteState.js
+++ src/Contexts/notes/noteState.js
@@ -19,24 +19,10 @@ import { useContext, useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props) => {
-    //creating state variables
-    const s1 = {
-        "name": "Harry",
-        "class": "5b"
-    }
-    const [state, setstate] = useState(s1);
-    const update = ()=>{
-        setTimeout(() => {
-            setstate({
-                "name": "Larry",
-                "class": "10b"
-            })
-        }, 1000);
-    }
     return (
         //anything wrapped in <noteState> </noteState> in app.js or any other component will have access to all the states, even the components childrens (down the hiearchy)
         //We can pass state, functions etc using context hook
-        <noteContext.Provider value={{state, update}}>
+        <noteContext.Provider value={{}}>
             {props.children}
         </noteContext.Provider>
     )
--- src/components/About.js
+++ src/components/About.js
@@ -3,16 +3,9 @@ import { useContext, useEffect } from 'react'
 import noteContext from '../Contexts/notes/noteContext'
 
 export default function About() {
-    //here noteContext is the provider of all the values passed and a is storing the values as object
-    const a = useContext(noteContext);
-
-    useEffect(() => {
-        a.update();
-    }, [])
-    
     return (
         <div>
-            this is about {a.state.name} and class {a.state.class}
+            this is about
         </div>
     )
 }
